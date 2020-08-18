e82c779f649ff96bf73082cabd7703b39702a573
Shaun Croucher <shcrouch@microsoft.com>
2020-08-18 13:11:51 +0100

    added physical and replication appliance

---
+++ repappliance.tf
@@ -0,0 +1,36 @@
+resource "azurerm_network_interface" "repappliance" {
+  name                = "repappliance-nic"
+  location            = azurerm_resource_group.onprem.location
+  resource_group_name = azurerm_resource_group.onprem.name
+ 
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.onprem.id
+    private_ip_address_allocation = "Dynamic"
+    
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "repappliance" {
+  name                = "repappliance-vm"
+  resource_group_name = azurerm_resource_group.onprem.name
+  location            = azurerm_resource_group.onprem.location
+  size                = "Standard_D2_v2"
+  admin_username      = var.vm_admin_user
+  admin_password      = var.vm_admin_pwd
+  network_interface_ids = [
+    azurerm_network_interface.repappliance.id
+    ]
+ 
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+ 
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
